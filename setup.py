b136d55f13a421be401e7afd2a4b3a900219aeb9
Victor <VMangeleer@student.uliege.be>
2023-05-09 12:06:43 +0200

    Fourier Flow (10) - Setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 from setuptools import find_packages, setup
 
 VERSION          = '1.0.0'
-DESCRIPTION      = 'Fourier Neural Operators'
-LONG_DESCRIPTION = 'Fourier Neural Operators - Contains the Fourier Neural Operator and the Factorized Fourier Neural Operator'
+DESCRIPTION      = 'Fourier Flow'
+LONG_DESCRIPTION = 'Fourier Flow - Contains the Fourier Neural Operator and the Factorized Fourier Neural Operator'
 
 setup(
       name             = "fourierflow",
       version          = VERSION,
-      url              = 'https://github.com/alasdairtran/fourierflow',
-      author           = 'Alasdair Tran',
-      author_email     = 'alasdair.tran@anu.edu.au',
+      url              = 'https://github.com/VikVador/FourierFlow/tree/main',
+      author           = 'Victor Mangeleer',
+      author_email     = 'vmangeleer@student.uliege.be',
       description      = DESCRIPTION,
       long_description = LONG_DESCRIPTION,
-      packages         = ["FourierFlow",
-                          "fourierflow",
+      packages         = ["fourierflow",
                           "fourierflow.FactorizedFourierNeuralOperator._2D_",
                           "fourierflow.FactorizedFourierNeuralOperator._3D_",
-                          "fourierflow.FourierNeuralOperator",
                           "fourierflow.FourierNeuralOperator._2D_",
                           "fourierflow.FourierNeuralOperator._3D_"],
 )
